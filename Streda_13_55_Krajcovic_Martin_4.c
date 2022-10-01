c77ed04de7ac5de2d6bbe54d0d47eb4249c2e275
mxrshall <martin22.krajcovic@gmail.com>
2022-10-01 18:33:46 +0200

    Trojuholnik

--- Streda_13_55_Krajcovic_Martin_4.c
+++ Streda_13_55_Krajcovic_Martin_4.c
@@ -5,13 +5,16 @@ int main(void)
 
 float a, b, c;
 
-printf("Zadaj strany trojuholnika:");
-scanf("%f" "%f" "%f", &a, &b, &c);
-
-if(a+b>c && b+c>a && a+c>b){
-    printf("Trojuholnikova nerovnost plati.");
-}else{
-    printf("Trojuholnikova nerovnost neplati.");
+do{
+    printf("Zadaj strany trojuholnika vacsie ako 0:\n");
+    scanf("%f" "%f" "%f", &a, &b, &c);
+}while(a<=0 || b<=0 || c<=0);
+{
+    if(a+b>c && b+c>a && a+c>b){
+        printf("Trojuholnikova nerovnost plati.\n");
+    }else{
+        printf("Trojuholnikova nerovnost neplati.\n");
+    }
 }
 
 return 0;
