0832740b570c808f5c23e8818860dfaa3ed581ed
mxrshall <martin22.krajcovic@gmail.com>
2022-10-05 20:58:02 +0200

    Kalkulacka Switch

--- Kalkulacka.c
+++ Kalkulacka.c
@@ -2,36 +2,53 @@
 
 int main(void){
 
-float num1 ,num2;
+float num1 ,num2, vysledok;
 char znak;
 
-do{
+printf("Zadaj prve cislo: ");
+scanf("%f", &num1);
+fflush(stdin);
+
+while(num1==0){
     printf("Zadaj prve cislo: ");
     scanf("%f", &num1);
-}while(num1<=0);
-
-do{
-    printf("Zadaj druhe cislo: ");
-    scanf("%f", &num2);
-}while(num2<=0);
-
-do{
-    printf("Zadaj znak: ");
-    scanf("%s", znak);
-}while(znak!='+' || znak!='-' || znak!='*' || znak!='/');
-
-if(znak=='+'){
-    printf("Vysledok je: %f", num1 + num2);
-}else if(znak=='-'){
-    printf("Vysledok je: %f", num1 - num2);
-}else if(znak=='*'){
-    printf("Vysledok je: %f", num1 * num2);
-}else if(znak=='/'){
-    printf("Vysledok je: %f", num1 / num2);
+    fflush(stdin);
 }
 
+printf("Zadaj znak: ");
+scanf("%c", &znak);
+fflush(stdin);
 
+while(znak!='+' && znak!='-' && znak!='*' && znak!='/'){
+    printf("Zadaj znak: ");
+    scanf("%c", &znak);
+    fflush(stdin);
+}
 
+printf("Zadaj druhe cislo: ");
+scanf("%f", &num2);
+
+while(num2==0){
+    printf("Zadaj druhe cislo: ");
+    scanf("%f", &num2);
+}
+
+switch(znak){
+    case '+':
+    vysledok=num1+num2;
+    break;
+    case '-':
+    vysledok=num1-num2;
+    break;
+    case '*':
+    vysledok=num1*num2;
+    break;
+    case '/':
+    vysledok=num1/num2;
+    break;
+}
+
+printf("Vysledok je: %f", vysledok);
 
 return 0;
 }
