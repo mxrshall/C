fcbc6e3e62b8829ccc4e6cd792726f98d252947b
mxrshall <martin22.krajcovic@gmail.com>
2022-10-10 16:59:55 +0200

    Typedef

--- Struct.c
+++ Struct.c
@@ -8,10 +8,16 @@ struct Student{
 
 };
 
+typedef struct{
+    int telefon;
+
+} praca;
+
 int main (){
 
 struct Student student1;
 struct Student student2;
+praca pracovnik1;
 
 strcpy(student1.meno, "Daniel");
 student1.vek = 15;
@@ -21,9 +27,12 @@ strcpy(student2.meno, "Matej");
 student2.vek = 17;
 student2.znamka = 3;
 
+pracovnik1.telefon = 1655;
+
 printf("Student1 sa vola %s", student1.meno);
 printf("Student1 ma %d", student1.vek);
 printf("Student2 sa vola %s", student2.meno);
+printf("Pracovnik1 ma cislo: %d", pracovnik1.telefon);
 
     return 0;
 }
