b0ceef47f5f5a92e55da5dc31e0fc1fecf90270a
mxrshall <martin22.krajcovic@gmail.com>
2022-10-14 14:53:37 +0200

    Cvicenie

--- Streda_13_55_Krajcovic_Martin_9.c
+++ Streda_13_55_Krajcovic_Martin_9.c
@@ -2,33 +2,40 @@
 
 int main(void){
 
-int a, b, i;
-int vysledok2=0;
-int vysledok3=0;
+int a, b, x;
+int vysledok2=0, vysledok3=0;
 
-printf("Zadaj cislo: ");
-scanf("%d %d", &a, &b);
+while (1){
+    printf("Zadaj cislo: ");
+    scanf("%d %d", &a, &b);
 
-if(a<b){
-    for(i=a; i<=b; i++){
-        if(i%2==0){
-            vysledok2++;
-        }else if(i%3==0){
-            vysledok3++;
-        }else continue;
+    if(a==0 || b==0){
+        goto end;
     }
-}else if(a>b){
-    for(i=b; i<=a; i++){
-        if(i%2==0){
-            vysledok2++;
-        }else if(i%3==0){
-            vysledok3++;
-        }else continue;
+
+    if(a>b){
+        x=a;
+        a=b;
+        b=x;
     }
+
+    if(a<b){
+        for(a; a<=b; a++){
+            if(a%2==0){
+                vysledok2++;
+            }
+            if(a%3==0){
+                vysledok3++;
+            }
+        }
+    }
+    printf("V postupnosti sa nachadza %d cisel delitelnych dvojkou.\n", vysledok2);
+    printf("V postupnosti sa nachadza %d cisel delitelnych trojkou.\n", vysledok3);
+
+    vysledok2=0;
+    vysledok3=0;
 }
 
-printf("%d", vysledok2);
-printf("%d", vysledok3);
-
+    end: //goto end;
     return 0;
 }
