6470d524f18048ad918ba8f59fefbafa95b3295a
mxrshall <martin22.krajcovic@gmail.com>
2022-10-20 22:07:37 +0200

    Uloha

--- Streda_13_55_Krajcovic_Martin.c
+++ Streda_13_55_Krajcovic_Martin.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#define N 10
 
 int main(void){
 
-int pocetbodov, i, x;
+int pocetbodov, i, x, y, body[N];
 
 printf("Zadaj pocet bodov: ");
 scanf("%d", &pocetbodov);
 
 for(i=0;i<pocetbodov;i++){
-    printf("Zadaj bod: ");
-    scanf("%d", &x);
+    printf("Zadaj bod X: ");
+    scanf("%d %d", &body[i]);
 }
 
-printf("Bod %d", x);
+for(i=0;i<pocetbodov;i++){
+    printf("%d", body[i]);
+}
 
     return 0;
 }
