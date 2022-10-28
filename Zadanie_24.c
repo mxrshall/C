062b4b7f6ee2daeb6967f41e28395d7cdbe4d27d
mxrshall <martin22.krajcovic@gmail.com>
2022-10-28 17:04:38 +0200

    Zadanie

---
+++ Zadanie_24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+int r, s, i;
+
+for(r=0;r<4;i++){
+    printf("Zadaj prvok do stpca matice [%d] [%d] [%d] [%d].", s, s, s, s);
+    scanf("%d %d %d %d", &s, &s, &s, &s);
+}
+
+    return 0;
+}
