ac2b16a1e9598c3dcbf238d6175ae7289f73e8e4
mxrshall <martin22.krajcovic@gmail.com>
2022-11-12 23:50:54 +0100

    Uloha

--- Streda_13_55_Krajcovic_Martin_4d.c
+++ Streda_13_55_Krajcovic_Martin_4d.c
@@ -14,23 +14,40 @@ vypocet(n);
 }
 
 void vypocet(int n){
-    int i, opakovanie;
-    long long int vysledok;
+    float i, opakovanie; 
+    double vysledok;
 
     for(i=1;i<=20;i++){
         if(n==0){
-            printf("%d mocnina z %d je 1\n", n, i);
+            printf("%d mocnina z %f je 1\n", n, i);
         }else if(n==1){
-            printf("%d mocnina z %d je %d\n", n, i, i);
+            printf("%d mocnina z %f je %f\n", n, i, i);
         }else if(n==2){
             vysledok=i*i;
-            printf("%d mocnina z %d je %lld\n", n, i, vysledok);
+            printf("%d mocnina z %f je %lf\n", n, i, vysledok);
+        }else if(n==-1){
+            vysledok=1/i;
+            printf("%d mocnina z %f je %lf\n", n, i, vysledok);
+        }else if(n==-2){
+            vysledok=i*i;
+            vysledok=1/vysledok;
+            printf("%d mocnina z %f je %lf\n", n, i, vysledok);
+        }else if(n<-2){
+            n=n*-1;
+            vysledok=i*i;
+            for(opakovanie=3;opakovanie<=n;opakovanie++){
+                vysledok=vysledok*i;
+            }
+            vysledok=1/vysledok;
+            printf("%d mocnina z %f je %lf\n", n, i, vysledok);
+            vysledok=0;
+            n=n*-1;
         }else{
             vysledok=i*i;
             for(opakovanie=3;opakovanie<=n;opakovanie++){
                 vysledok=vysledok*i;
             }
-            printf("%d mocnina z %d je %lld\n", n, i, vysledok);
+            printf("%d mocnina z %f je %lf\n", n, i, vysledok);
             vysledok=0;
         }
         
