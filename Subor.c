1fe6ed759e08b4a34511fa4f2883065f8cac38ae
mxrshall <martin22.krajcovic@gmail.com>
2022-11-20 00:38:33 +0100

    Subor

--- Subor.c
+++ Subor.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
+#define MAX 100
 
 int main(void){
 
 FILE *f_output;
-int cislo = 4, i;
+int cislo = 4;
+float mat[MAX][MAX];
+int i, j;
+
+for (i=0;i<2;i++){
+    for (j=0;j<2;j++){
+        printf("Zadaj prvok matice [%d][%d]\n",i,j);
+        scanf("%f",&mat[i][j]);
+    }
+}
 
 f_output = fopen("file.txt", "w");
 
 fputs("Ahoj\n", f_output);
 fprintf(f_output, "Tvoje cislo %d\n", cislo);
 
-for(i=0;i<=10;i++){
-  fprintf(f_output, "Tvoje cislo %d\n", i);  
+fprintf(f_output, "Toto je tvoja matica.\n");
+for (i=0;i<2;i++){
+    for (j=0;j<2;j++){
+        fprintf(f_output, "%f\t", mat[i][j]);    
+    }
+    printf("\n");
 }
+
 fclose(f_output);
 
 FILE *f_input;
 
 f_input = fopen("file.txt", "r");
 
-fscanf(f_input, "%d", &i);
-printf("%d", i);
+for(i=0;i<=10;i++){
+  fscanf(f_input, "%d", &i);
+  printf("%d", i); 
+}
 
 fclose(f_input);
 
