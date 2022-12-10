c01b7e3e44255360519e1bc2fb90801fc647832b
mxrshall <martin22.krajcovic@gmail.com>
2022-12-10 15:04:34 +0100

    Pointers

--- Pointers.c
+++ Pointers.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
+int vypocet(int *pX, int *pY);
+
 int main(void){
-
-int x = 4;
+int x;
+int y;
 int *pX = &x;
-int y = *pX;
+int *pY = &y;
 
-printf("%d", y);
+printf("Zadaj prve cislo: ");
+scanf("%d", &x);
+
+printf("Zadaj druhe cislo: ");
+scanf("%d", &y);
+
+printf("Vacsie cislo je %d", vypocet(pX, pY));
 
     return 0;
+}
+
+int vypocet(int *pX, int *pY){
+
+if(pX<pY){
+    return *pY;
+}
+
+return *pX;
+
 }
