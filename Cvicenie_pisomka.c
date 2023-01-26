85f7839203cf9eaecb13bcc3a7886b66d57f25f6
mxrshall <martin22.krajcovic@gmail.com>
2023-01-26 16:14:57 +0100

    C

--- Cvicenie_pisomka.c
+++ Cvicenie_pisomka.c
@@ -1,39 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-void function(int *pole, int rozmer);
+#include<string.h>
 
 int main(){
 
-int rozmer, *pole, i;
+char *s;
 
-do{
-printf("Zadaj rozmer: ");
-scanf("%d", &rozmer);
-}while(rozmer<3);
-
-pole=(int*)malloc(rozmer*sizeof(int));
-
-for(i=0;i<rozmer;i++){
-    printf("Zadaj cislo: ");
-    scanf("%d", &pole[i]);  
-}
-
-function(pole, rozmer);
+printf("Zadaj text: ");
+gets(s);
+int dlzka = strlen(s);
+s=(char*)malloc(dlzka*sizeof(char));
 
     return 0;
-}
-
-void function(int *pole, int rozmer){
-
-int i, vysledok;
-
-for(i=0;i<rozmer;i++){
-    printf("%d", pole[i]);
-}
-
-vysledok = pole[0] + pole[1] + pole[2];
-
-printf("\nVysledok: %d", vysledok);
-
 }
